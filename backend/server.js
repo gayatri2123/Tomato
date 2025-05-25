85811f94b44cd8a98ffb555bb37d8a9deb8bb042
gayatri2123 <130831849+gayatri2123@users.noreply.github.com>
2025-05-26 00:39:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ const port = process.env.Port || 4000;
 
 //middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: "https://food-del-frontend-kqpk.onrender.com",
+  credentials: true
+}));
+
 
 //db connecrion
 connectDB();
